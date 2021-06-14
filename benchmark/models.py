ca3a0e32813e7151449f0efc8772f39875ea8c0e
Jannes Münchmeyer <munchmej@gfz-potsdam.de>
2021-06-14 17:33:34 +0200

    Implemented basic EQTransformer model

--- benchmark/models.py
+++ benchmark/models.py
@@ -27,6 +27,21 @@ phase_dict = {
 }
 
 
+def vector_cross_entropy(y_pred, y_true, eps=1e-5):
+    """
+    Cross entropy loss
+
+    :param y_true: True label probabilities
+    :param y_pred: Predicted label probabilities
+    :param eps: Epsilon to clip values for stability
+    :return: Average loss across batch
+    """
+    h = y_true * torch.log(y_pred + eps)
+    h = h.mean(-1).sum(-1)  # Mean along sample dimension and sum along pick dimension
+    h = h.mean()  # Mean over batch axis
+    return -h
+
+
 class SeisBenchModuleLit(pl.LightningModule, ABC):
     """
     Abstract interface for SeisBench lightning modules.
@@ -59,6 +74,7 @@ class PhaseNetLit(pl.LightningModule):
         self.lr = lr
         self.sigma = sigma
         self.sample_boundaries = sample_boundaries
+        self.loss = vector_cross_entropy
         self.model = sbm.PhaseNet(**kwargs)
 
     def forward(self, x):
@@ -68,7 +84,7 @@ class PhaseNetLit(pl.LightningModule):
         x = batch["X"]
         y_true = batch["y"]
         y_pred = self.model(x)
-        return self.loss(y_true, y_pred)
+        return self.loss(y_pred, y_true)
 
     def training_step(self, batch, batch_idx):
         loss = self.shared_step(batch)
@@ -79,23 +95,6 @@ class PhaseNetLit(pl.LightningModule):
         loss = self.shared_step(batch)
         self.log("val_loss", loss)
         return loss
-
-    @staticmethod
-    def loss(y_true, y_pred, eps=1e-5):
-        """
-        Cross entropy loss
-
-        :param y_true: True label probabilities
-        :param y_pred: Predicted label probabilities
-        :param eps: Epsilon to clip values for stability
-        :return: Average loss across batch
-        """
-        h = y_true * torch.log(y_pred + eps)
-        h = h.mean(-1).sum(
-            -1
-        )  # Mean along sample dimension and sum along pick dimension
-        h = h.mean()  # Mean over batch axis
-        return -h
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
@@ -212,3 +211,98 @@ class GPDLit(pl.LightningModule):
             + filter
             + [sbg.ChangeDtype(np.float32)]
         )
+
+
+class EQTransformerLit(pl.LightningModule):
+    """
+    LightningModule for PhaseNet
+
+    :param lr: Learning rate, defaults to 1e-2
+    :param sigma: Standard deviation passed to the ProbabilisticPickLabeller
+    :param sample_boundaries: Low and high boundaries for the RandomWindow selection.
+    :param kwargs: Kwargs are passed to the SeisBench.models.PhaseNet constructor.
+    """
+
+    def __init__(
+        self,
+        lr=1e-2,
+        sigma=10,
+        sample_boundaries=(None, None),
+        loss_weights=(0.05, 0.40, 0.55),
+        **kwargs
+    ):
+        super().__init__()
+        self.save_hyperparameters()
+        self.lr = lr
+        self.sigma = sigma
+        self.sample_boundaries = sample_boundaries
+        self.loss = torch.nn.BCELoss()
+        self.loss_weights = loss_weights
+        self.model = sbm.EQTransformer(**kwargs)
+
+    def forward(self, x):
+        return self.model(x)
+
+    def shared_step(self, batch):
+        x = batch["X"]
+        p_true = batch["y"][:, 0]
+        s_true = batch["y"][:, 1]
+        det_true = batch["detection"][:, 0]
+        det_pred, p_pred, s_pred = self.model(x)
+
+        return (
+            self.loss_weights[0] * self.loss(det_pred, det_true)
+            + self.loss_weights[1] * self.loss(p_pred, p_true)
+            + self.loss_weights[2] * self.loss(s_pred, s_true)
+        )
+
+    def training_step(self, batch, batch_idx):
+        loss = self.shared_step(batch)
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self.shared_step(batch)
+        self.log("val_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        return optimizer
+
+    def get_augmentations(self):
+        p_phases = [key for key, val in phase_dict.items() if val == "P"]
+        s_phases = [key for key, val in phase_dict.items() if val == "S"]
+
+        return [
+            # In 2/3 of the cases, select windows around picks, to reduce amount of noise traces in training.
+            # Uses strategy variable, as padding will be handled by the random window.
+            # In 1/3 of the cases, just returns the original trace, to keep diversity high.
+            sbg.OneOf(
+                [
+                    sbg.WindowAroundSample(
+                        list(phase_dict.keys()),
+                        samples_before=6000,
+                        windowlen=12000,
+                        selection="random",
+                        strategy="variable",
+                    ),
+                    sbg.NullAugmentation(),
+                ],
+                probabilities=[2, 1],
+            ),
+            sbg.RandomWindow(
+                low=self.sample_boundaries[0],
+                high=self.sample_boundaries[1],
+                windowlen=6000,
+                strategy="pad",
+            ),
+            sbg.Normalize(demean_axis=-1, amp_norm_axis=-1, amp_norm_type="peak"),
+            sbg.ProbabilisticLabeller(
+                label_columns=phase_dict, sigma=self.sigma, dim=0
+            ),
+            sbg.DetectionLabeller(p_phases, s_phases, key=("X", "detection")),
+            sbg.ChangeDtype(np.float32, "X"),
+            sbg.ChangeDtype(np.float32, "y"),
+            sbg.ChangeDtype(np.float32, "detection"),
+        ]
