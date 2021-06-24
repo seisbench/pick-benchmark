9c26f94c3365732e694d4b53bee4565799aa266a
Jannes Münchmeyer <munchmej@gfz-potsdam.de>
2021-06-24 15:54:41 +0200

    BasicPhaseAELit and evaluation
    Includes predict_step for PhaseNet and EQTransformer

---
+++ benchmark/eval.py
@@ -0,0 +1,155 @@
+import seisbench.generate as sbg
+
+import argparse
+import pandas as pd
+import numpy as np
+import yaml
+from pathlib import Path
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader
+import torch
+
+import models
+import data
+import logging
+
+
+def main(weights, targets, sets, batchsize, num_workers):
+    weights = Path(weights)
+    targets = Path(targets)
+    sets = sets.split(",")
+
+    version = sorted(weights.iterdir())[-1]
+    config_path = version / "hparams.yaml"
+    with open(config_path, "r") as f:
+        # config = yaml.safe_load(f)
+        config = yaml.load(f)
+
+    model_cls = models.__getattribute__(config["model"] + "Lit")
+    model = load_best_model(model_cls, weights, version.name)
+
+    dataset = data.get_dataset_by_name(config["data"])(
+        sampling_rate=100, component_order="ZNE", dimension_order="NCW", cache="full"
+    )
+    for eval_set in sets:
+        split = dataset.get_split(eval_set)
+        logging.warning(f"Starting set {eval_set}")
+        split.preload_waveforms(pbar=True)
+
+        for task in ["1", "23"]:
+            task_csv = targets / f"task{task}.csv"
+
+            if not task_csv.is_file():
+                continue
+
+            logging.warning(f"Starting task {task}")
+
+            task_targets = pd.read_csv(task_csv)
+            task_targets = task_targets[task_targets["trace_split"] == eval_set]
+
+            generator = sbg.SteeredGenerator(split, task_targets)
+            generator.add_augmentations(model.get_eval_augmentations())
+
+            loader = DataLoader(
+                generator, batch_size=batchsize, shuffle=False, num_workers=num_workers
+            )
+
+            trainer = pl.Trainer(gpus=1)
+
+            predictions = trainer.predict(model, loader)
+
+            # Merge batches
+            merged_predictions = []
+            for i, _ in enumerate(predictions[0]):
+                merged_predictions.append(torch.cat([x[i] for x in predictions]))
+
+            merged_predictions = [x.cpu().numpy() for x in merged_predictions]
+            task_targets["score_detection"] = merged_predictions[0]
+            task_targets["score_p_or_s"] = merged_predictions[1]
+            task_targets["p_sample_pred"] = (
+                merged_predictions[2] + task_targets["start_sample"]
+            )
+            task_targets["s_sample_pred"] = (
+                merged_predictions[3] + task_targets["start_sample"]
+            )
+
+            pred_path = (
+                weights.parent.parent
+                / "pred"
+                / weights.name
+                / version.name
+                / f"{eval_set}_task{task}.csv"
+            )
+            pred_path.parent.mkdir(exist_ok=True, parents=True)
+            task_targets.to_csv(pred_path, index=False)
+
+
+def load_best_model(model_cls, weights, version):
+    """
+    Determines the model with lowest validation loss from the csv logs and loads it
+
+    :param model_cls: Class of the lightning module to load
+    :param weights: Path to weights as in cmd arguments
+    :param version: String of version file
+    :return: Instance of lightning module that was loaded from the best checkpoint
+    """
+    metrics = pd.read_csv(weights / version / "metrics.csv")
+
+    idx = np.nanargmin(metrics["val_loss"])
+    min_row = metrics.iloc[idx]
+
+    checkpoint = f"epoch={min_row['epoch']:.0f}-step={min_row['step']:.0f}.ckpt"
+
+    version_id = version.split("_")[-1]
+    version_str = f"{version_id}_{version_id}"
+
+    checkpoint_path = (
+        weights.parent
+        / f"{weights.name}_{weights.name}"
+        / version_str
+        / "checkpoints"
+        / checkpoint
+    )
+
+    return model_cls.load_from_checkpoint(checkpoint_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Evaluate a trained model using a set of targets."
+    )
+    parser.add_argument(
+        "weights",
+        type=str,
+        help="Path to weights. "
+        "The script will automatically load the configuration and the model. "
+        "The script always uses the newest version and the checkpoint with lowest validation loss."
+        "Predictions will be written into the weights path as csv."
+        "Note: Due to pytorch lightning internals, there exist two weights folders, "
+        "{weights} and {weight}_{weights}. Please use the former as parameter",
+    )
+    parser.add_argument(
+        "targets",
+        type=str,
+        help="Path to evaluation targets folder. "
+        "The script will detect which tasks are present base on file names.",
+    )
+    parser.add_argument(
+        "--sets",
+        type=str,
+        default="dev,test",
+        help="Sets on which to evaluate, separated by commata. Defaults to dev and test.",
+    )
+    parser.add_argument("--batchsize", default=1024, help="Batch size")
+    parser.add_argument(
+        "--num_workers", default=12, help="Number of workers for data loader"
+    )
+    args = parser.parse_args()
+
+    main(
+        args.weights,
+        args.targets,
+        args.sets,
+        batchsize=args.batchsize,
+        num_workers=args.num_workers,
+    )
--- benchmark/models.py
+++ benchmark/models.py
@@ -64,10 +64,44 @@ class SeisBenchModuleLit(pl.LightningModule, ABC):
         pass
 
     def get_train_augmentations(self):
+        """
+        Returns the set of training augmentations.
+        """
         return self.get_augmentations()
 
     def get_val_augmentations(self):
+        """
+        Returns the set of validation augmentations for validations during training.
+        """
         return self.get_augmentations()
+
+    @abstractmethod
+    def get_eval_augmentations(self):
+        """
+        Returns the set of evaluation augmentations for evaluation after training.
+        These augmentations will be passed to a SteeredGenerator and should usually contain a steered window.
+        """
+        pass
+
+    @abstractmethod
+    def predict_step(self, batch, batch_idx=None, dataloader_idx=None):
+        """
+        Predict step for the lightning module. Returns results for three tasks:
+
+        - earthquake detection (score, higher means more likely detection)
+        - P to S phase discrimination (score, high means P, low means S)
+        - phase location in samples (two integers, first for P, second for S wave)
+
+        All predictions should only take the window defined by batch["window_borders"] into account.
+
+        :param batch:
+        :return:
+        """
+        score_detection = None
+        score_p_or_s = None
+        p_sample = None
+        s_sample = None
+        return score_detection, score_p_or_s, p_sample, s_sample
 
 
 class PhaseNetLit(SeisBenchModuleLit):
@@ -142,6 +176,38 @@ class PhaseNetLit(SeisBenchModuleLit):
                 label_columns=phase_dict, sigma=self.sigma, dim=0
             ),
         ]
+
+    def get_eval_augmentations(self):
+        return [
+            sbg.SteeredWindow(windowlen=3001, strategy="pad"),
+            sbg.ChangeDtype(np.float32),
+            sbg.Normalize(demean_axis=-1, amp_norm_axis=-1, amp_norm_type="peak"),
+        ]
+
+    def predict_step(self, batch, batch_idx=None, dataloader_idx=None):
+        x = batch["X"]
+        window_borders = batch["window_borders"]
+
+        pred = self.model(x)
+
+        score_detection = torch.zeros(pred.shape[0])
+        score_p_or_s = torch.zeros(pred.shape[0])
+        p_sample = torch.zeros(pred.shape[0], dtype=int)
+        s_sample = torch.zeros(pred.shape[0], dtype=int)
+
+        for i in range(pred.shape[0]):
+            start_sample, end_sample = window_borders[i]
+            local_pred = pred[i, :, start_sample:end_sample]
+
+            score_detection[i] = torch.max(1 - local_pred[-1])  # 1 - noise
+            score_p_or_s[i] = torch.max(local_pred[0]) / torch.max(
+                local_pred[1]
+            )  # most likely P by most likely S
+
+            p_sample[i] = torch.argmax(local_pred[0])
+            s_sample[i] = torch.argmax(local_pred[1])
+
+        return score_detection, score_p_or_s, p_sample, s_sample
 
 
 class GPDLit(SeisBenchModuleLit):
@@ -223,6 +289,12 @@ class GPDLit(SeisBenchModuleLit):
             + filter
             + [sbg.ChangeDtype(np.float32)]
         )
+
+    def get_eval_augmentations(self):
+        raise NotImplementedError()
+
+    def predict_step(self, batch, batch_idx=None, dataloader_idx=None):
+        raise NotImplementedError()
 
 
 class EQTransformerLit(SeisBenchModuleLit):
@@ -387,6 +459,40 @@ class EQTransformerLit(SeisBenchModuleLit):
     def get_augmentations(self):
         raise NotImplementedError("Use get_train/val_augmentations instead.")
 
+    def get_eval_augmentations(self):
+        return [
+            sbg.SteeredWindow(windowlen=6000, strategy="pad"),
+            sbg.ChangeDtype(np.float32),
+            sbg.Normalize(detrend_axis=-1, amp_norm_axis=-1, amp_norm_type="peak"),
+        ]
+
+    def predict_step(self, batch, batch_idx=None, dataloader_idx=None):
+        x = batch["X"]
+        window_borders = batch["window_borders"]
+
+        det_pred, p_pred, s_pred = self.model(x)
+
+        score_detection = torch.zeros(det_pred.shape[0])
+        score_p_or_s = torch.zeros(det_pred.shape[0])
+        p_sample = torch.zeros(det_pred.shape[0], dtype=int)
+        s_sample = torch.zeros(det_pred.shape[0], dtype=int)
+
+        for i in range(det_pred.shape[0]):
+            start_sample, end_sample = window_borders[i]
+            local_det_pred = det_pred[i, start_sample:end_sample]
+            local_p_pred = p_pred[i, start_sample:end_sample]
+            local_s_pred = s_pred[i, start_sample:end_sample]
+
+            score_detection[i] = torch.max(local_det_pred)
+            score_p_or_s[i] = torch.max(local_p_pred) / torch.max(
+                local_s_pred
+            )  # most likely P by most likely S
+
+            p_sample[i] = torch.argmax(local_p_pred)
+            s_sample[i] = torch.argmax(local_s_pred)
+
+        return score_detection, score_p_or_s, p_sample, s_sample
+
 
 class CREDLit(SeisBenchModuleLit):
     """
@@ -480,3 +586,89 @@ class CREDLit(SeisBenchModuleLit):
         ]
 
         return augmentations
+
+    def get_eval_augmentations(self):
+        raise NotImplementedError()
+
+    def predict_step(self, batch, batch_idx=None, dataloader_idx=None):
+        raise NotImplementedError()
+
+
+class BasicPhaseAELit(SeisBenchModuleLit):
+    """
+    LightningModule for BasicPhaseAE
+
+    :param lr: Learning rate, defaults to 1e-2
+    :param sigma: Standard deviation passed to the ProbabilisticPickLabeller
+    :param sample_boundaries: Low and high boundaries for the RandomWindow selection.
+    :param kwargs: Kwargs are passed to the SeisBench.models.BasicPhaseAE constructor.
+    """
+
+    def __init__(self, lr=1e-2, sigma=10, sample_boundaries=(None, None), **kwargs):
+        super().__init__()
+        self.save_hyperparameters()
+        self.lr = lr
+        self.sigma = sigma
+        self.sample_boundaries = sample_boundaries
+        self.loss = vector_cross_entropy
+        self.model = sbm.BasicPhaseAE(**kwargs)
+
+    def forward(self, x):
+        return self.model(x)
+
+    def shared_step(self, batch):
+        x = batch["X"]
+        y_true = batch["y"]
+        y_pred = self.model(x)
+        return self.loss(y_pred, y_true)
+
+    def training_step(self, batch, batch_idx):
+        loss = self.shared_step(batch)
+        self.log("train_loss", loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self.shared_step(batch)
+        self.log("val_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
+        return optimizer
+
+    def get_augmentations(self):
+        return [
+            # In 2/3 of the cases, select windows around picks, to reduce amount of noise traces in training.
+            # Uses strategy variable, as padding will be handled by the random window.
+            # In 1/3 of the cases, just returns the original trace, to keep diversity high.
+            sbg.OneOf(
+                [
+                    sbg.WindowAroundSample(
+                        list(phase_dict.keys()),
+                        samples_before=600,
+                        windowlen=600,
+                        selection="random",
+                        strategy="variable",
+                    ),
+                    sbg.NullAugmentation(),
+                ],
+                probabilities=[2, 1],
+            ),
+            sbg.RandomWindow(
+                low=self.sample_boundaries[0],
+                high=self.sample_boundaries[1],
+                windowlen=600,
+                strategy="pad",
+            ),
+            sbg.ChangeDtype(np.float32),
+            sbg.Normalize(demean_axis=-1, amp_norm_axis=-1, amp_norm_type="peak"),
+            sbg.ProbabilisticLabeller(
+                label_columns=phase_dict, sigma=self.sigma, dim=0
+            ),
+        ]
+
+    def get_eval_augmentations(self):
+        raise NotImplementedError()
+
+    def predict_step(self, batch, batch_idx=None, dataloader_idx=None):
+        raise NotImplementedError()
