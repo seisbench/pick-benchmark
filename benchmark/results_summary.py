1180af343fff23c41d03abbd25c0c8a3fea0b27b
Jannes Münchmeyer <munchmej@gfz-potsdam.de>
2021-07-30 15:46:07 +0200

    Results summary tables and plots for cross-domain experiments

--- benchmark/results_summary.py
+++ benchmark/results_summary.py
@@ -20,6 +20,83 @@ def main():
     print("Generating plots")
     results_plots(results)
 
+    results_cross = pd.read_csv("results_cross.csv")
+    # Add "diagonal" entries
+    results["target"] = results["data"]
+    results_cross = results_cross.append(results)
+
+    for model in results["model"].unique():
+        model_results(results_cross, model)
+
+
+def model_results(results_cross, model):
+    print(f"Generating {model} results")
+    results_model = results_cross[results_cross["model"] == model]
+
+    table = results_to_table(
+        results_model,
+        ["test_det_precision", "test_det_recall", "test_det_f1"],
+        "dev_det_f1",
+        ["P", "R", "F1"],
+        axis=("data", "target"),
+    )
+    with open(f"results/cross/{model}_detection_test.tex", "w") as f:
+        f.write(table)
+
+    table = results_to_table(
+        results_model[results_model["data"] != "lendb"],
+        ["test_phase_precision", "test_phase_recall", "test_phase_f1"],
+        "dev_phase_f1",
+        ["P", "R", "F1"],
+        axis=("data", "target"),
+    )
+    with open(f"results/cross/{model}_phase_test.tex", "w") as f:
+        f.write(table)
+
+    table = results_to_table(
+        results_model,
+        ["test_P_mean_s", "test_P_std_s", "test_P_mae_s"],
+        "dev_P_std_s",
+        ["$\\mu$", "$\\sigma$", "MAE"],
+        minimize=True,
+        average=[1, 2],
+        axis=("data", "target"),
+    )
+    with open(f"results/cross/{model}_precision_p_test.tex", "w") as f:
+        f.write(table)
+
+    table = results_to_table(
+        results_model[results_model["data"] != "lendb"],
+        ["test_S_mean_s", "test_S_std_s", "test_S_mae_s"],
+        "dev_S_std_s",
+        ["$\\mu$", "$\\sigma$", "MAE"],
+        minimize=True,
+        average=[1, 2],
+        axis=("data", "target"),
+    )
+    with open(f"results/cross/{model}_precision_s_test.tex", "w") as f:
+        f.write(table)
+
+    fig = residual_matrix(
+        "P",
+        results_model,
+        [Path("pred"), Path("pred_cross")],
+        "dev_P_std_s",
+        axis=("data", "target"),
+    )
+    fig.savefig(f"results/cross/{model}_test_P_diff.eps", bbox_inches="tight")
+    plt.close(fig)
+
+    fig = residual_matrix(
+        "S",
+        results_model[results_model["data"] != "lendb"],
+        [Path("pred"), Path("pred_cross")],
+        "dev_S_std_s",
+        axis=("data", "target"),
+    )
+    fig.savefig(f"results/cross/{model}_test_S_diff.eps", bbox_inches="tight")
+    plt.close(fig)
+
 
 def results_tables(results):
     table = results_to_table(
@@ -66,9 +143,11 @@ def results_tables(results):
 def results_plots(results):
     fig = residual_matrix("P", results, Path("pred"), "dev_P_std_s")
     fig.savefig("results/test_P_diff.eps", bbox_inches="tight")
+    plt.close(fig)
 
     fig = residual_matrix("S", results, Path("pred"), "dev_S_std_s")
     fig.savefig("results/test_S_diff.eps", bbox_inches="tight")
+    plt.close(fig)
 
 
 def detect_missing_entries(results):
@@ -89,7 +168,7 @@ def detect_missing_entries(results):
                     print(np.sum(mask), data, model, lr)
 
 
-def results_to_array(results, cols, selection, minimize=False):
+def results_to_array(results, cols, selection, minimize=False, axis=("data", "model")):
     """
     Packs results into numpy array with axis data, model, cols.
 
@@ -98,17 +177,20 @@ def results_to_array(results, cols, selection, minimize=False):
     :param selection: Column to use for selecting the best model configuration
                       (in case of multiple learning rates, ...)
     :param minimize: If true, chooses the minimal value from column selection, otherwise the maximal.
+    :param axis: First two axis of the array, by default "data" and "model"
     :return: numpy array with results
     """
-    data_dict = {data: i for i, data in enumerate(results["data"].unique())}
-    model_dict = {model: i for i, model in enumerate(results["model"].unique())}
+    ax0, ax1 = axis
 
-    n_data = len(results["data"].unique())
-    n_model = len(results["model"].unique())
+    data_dict = {data: i for i, data in enumerate(sorted(results[ax0].unique()))}
+    model_dict = {model: i for i, model in enumerate(sorted(results[ax1].unique()))}
+
+    n_data = len(results[ax0].unique())
+    n_model = len(results[ax1].unique())
 
     res_array = np.nan * np.zeros((n_data, n_model, len(cols)))
 
-    for _, subdf in results.groupby(["data", "model"]):
+    for _, subdf in results.groupby([ax0, ax1]):
         if np.isnan(subdf[selection]).all():
             continue
         if minimize:
@@ -116,7 +198,7 @@ def results_to_array(results, cols, selection, minimize=False):
         else:
             i = np.nanargmax(subdf[selection])
         row = subdf.iloc[i]
-        entry = res_array[data_dict[row["data"]], model_dict[row["model"]]]
+        entry = res_array[data_dict[row[ax0]], model_dict[row[ax1]]]
         for col_idx, col in enumerate(cols):
             entry[col_idx] = row[col]
 
@@ -124,7 +206,13 @@ def results_to_array(results, cols, selection, minimize=False):
 
 
 def results_to_table(
-    results, cols, selection, labels=None, minimize=False, average=None
+    results,
+    cols,
+    selection,
+    labels=None,
+    minimize=False,
+    average=None,
+    axis=("data", "model"),
 ):
     """
     Format results as latex table
@@ -137,22 +225,24 @@ def results_to_table(
     :param minimize: If true, chooses the minimal value from column selection, otherwise the maximal.
     :param average: Indices for the columns that should have averages attached.
                     If None, outputs averages for all columns.
+    :param axis: Axis of the table, by default "data" and "model"
     :return: String for table
     """
+    ax0, ax1 = axis
     data_dict, model_dict, res_array = results_to_array(
-        results, cols, selection, minimize=minimize
+        results, cols, selection, minimize=minimize, axis=axis
     )
 
     if average is None:
         average = list(range(len(cols)))
 
-    n_data = len(results["data"].unique())
-    n_model = len(results["model"].unique())
+    n_data = len(results[ax0].unique())
+    n_model = len(results[ax1].unique())
 
     inv_data_dict = {v: k for k, v in data_dict.items()}
     inv_model_dict = {v: k for k, v in model_dict.items()}
 
-    header = ""
+    header = f"\\backslashbox{{{ax0}}}{{{ax1}}}"
     header_count = 0
     for j in range(n_model):
         if np.isnan(res_array[:, j]).all():
@@ -216,31 +306,41 @@ def results_to_table(
     return "\n".join(table_str)
 
 
-def residual_matrix(phase, results, pred_path, selection, lim=2):
+def residual_matrix(
+    phase, results, pred_path, selection, lim=2, axis=("data", "model")
+):
     """
     Plots pick residual distributions for each model and dataset in a grid
 
     :param phase: Seismic phase to plot
     :param results: Results dataframe
-    :param pred_path: Path to predictions
+    :param pred_path: Path to predictions or list of path to predictions
     :param selection: Column to use for selecting the best model configuration
                       (in case of multiple learning rates, ...)
     :param lim: Plotting limit for the distributions
+    :param axis: Axis of the grid, by default "data" and "model"
     :return: Matplotlib figure handle
     """
+    if not isinstance(pred_path, list):
+        pred_path = [pred_path]
+
+    ax0, ax1 = axis
     data_dict, model_dict, res_array = results_to_array(
-        results, [f"test_{phase}_mean_s"], selection, minimize=True
+        results, [f"test_{phase}_mean_s"], selection, minimize=True, axis=axis
     )
     res_array = res_array[:, :, 0]
 
-    n_data = len(results["data"].unique())
-    n_model = len(results["model"].unique())
+    n_data = len(results[ax0].unique())
+    n_model = len(results[ax1].unique())
 
     inv_data_dict = {v: k for k, v in data_dict.items()}
     inv_model_dict = {v: k for k, v in model_dict.items()}
 
     true_n_data = np.sum((~np.isnan(res_array)).any(axis=1))
     true_n_model = np.sum((~np.isnan(res_array)).any(axis=0))
+
+    if true_n_data == 0 or true_n_model == 0:
+        return plt.figure()
 
     fig = plt.figure(figsize=(15, 15))
     axs = fig.subplots(
@@ -260,21 +360,26 @@ def residual_matrix(phase, results, pred_path, selection, lim=2):
             if np.isnan(res_array[:, j]).all():
                 continue
             data, model = inv_data_dict[i], inv_model_dict[j]
-            mask = np.logical_and(results["model"] == model, results["data"] == data)
+            mask = np.logical_and(results[ax1] == model, results[ax0] == data)
 
             subdf = results[mask]
+            if np.isnan(subdf[selection]).all():
+                continue
             lr_idx = np.nanargmin(subdf[selection])
             row = subdf.iloc[lr_idx]
 
             axs[true_i, true_j].set_yticklabels([])
             axs[true_i, true_j].set_xlim(-lim, lim)
 
-            pred_path_loc = (
-                pred_path
-                / row["experiment"]
-                / f"version_{row['version']}"
-                / "test_task23.csv"
-            )
+            for pred_path_member in pred_path:
+                pred_path_loc = (
+                    pred_path_member
+                    / row["experiment"]
+                    / f"version_{row['version']}"
+                    / "test_task23.csv"
+                )
+                if pred_path_loc.is_file():
+                    break
             if not pred_path_loc.is_file():
                 true_j += 1
                 continue
@@ -314,6 +419,11 @@ def residual_matrix(phase, results, pred_path, selection, lim=2):
 
     for ax in axs[-1]:
         ax.set_xlabel("$t_{pred} - t_{true}$")
+
+    mid_left_ax = axs[axs.shape[0] // 2, 0]
+    mid_left_ax.set_ylabel(ax0 + "\n" + mid_left_ax.get_ylabel())
+    mid_top_ax = axs[0, axs.shape[1] // 2]
+    mid_top_ax.set_title(ax1 + "\n" + mid_top_ax.get_title())
 
     return fig
 
