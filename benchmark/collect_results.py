9eb8f40543c32835c6c7b393d22c66a7d4668b6e
Jannes Münchmeyer <munchmej@gfz-potsdam.de>
2021-08-04 12:16:17 +0200

    Minor fixes for results collection

--- benchmark/collect_results.py
+++ benchmark/collect_results.py
@@ -150,49 +150,65 @@ def eval_task23(version_dir: Path):
     test_pred = pd.read_csv(version_dir / "test_task23.csv")
     test_pred["phase_label_bin"] = test_pred["phase_label"] == "P"
 
-    if np.isnan(dev_pred["score_p_or_s"]).all():
+    def nanmask(pred):
+        """
+        Returns all entries that are nan in score_p_or_s, p_sample_pred and s_sample_pred
+        """
+        mask = np.logical_and(
+            np.isnan(pred["p_sample_pred"]), np.isnan(pred["s_sample_pred"])
+        )
+        mask = np.logical_and(mask, np.isnan(pred["score_p_or_s"]))
+        return mask
+
+    if nanmask(dev_pred).all():
         logging.warning(f"{version_dir} contains NaN predictions for tasks 2 and 3")
         return {}
 
-    dev_pred = dev_pred[~np.isnan(dev_pred["score_p_or_s"])]
-    test_pred = test_pred[~np.isnan(test_pred["score_p_or_s"])]
+    dev_pred = dev_pred[~nanmask(dev_pred)]
+    test_pred = test_pred[~nanmask(test_pred)]
     # Clipping removes infinitely likely P waves, usually resulting from models trained without S arrivals
     dev_pred["score_p_or_s"] = np.clip(dev_pred["score_p_or_s"].values, -1e100, 1e100)
     test_pred["score_p_or_s"] = np.clip(test_pred["score_p_or_s"].values, -1e100, 1e100)
 
-    prec, recall, thr = precision_recall_curve(
-        dev_pred["phase_label_bin"], dev_pred["score_p_or_s"]
-    )
+    dev_pred_restricted = dev_pred[~np.isnan(dev_pred["score_p_or_s"])]
+    test_pred_restricted = test_pred[~np.isnan(test_pred["score_p_or_s"])]
+    if len(dev_pred_restricted) > 0:
+        prec, recall, thr = precision_recall_curve(
+            dev_pred_restricted["phase_label_bin"], dev_pred_restricted["score_p_or_s"]
+        )
 
-    f1 = 2 * prec * recall / (prec + recall)
+        f1 = 2 * prec * recall / (prec + recall)
 
-    opt_index = np.nanargmax(f1)  # F1 optimal threshold index
-    opt_thr = thr[opt_index]  # F1 optimal threshold value
+        opt_index = np.nanargmax(f1)  # F1 optimal threshold index
+        opt_thr = thr[opt_index]  # F1 optimal threshold value
 
-    dev_stats = {
-        "dev_phase_precision": prec[opt_index],
-        "dev_phase_recall": recall[opt_index],
-        "dev_phase_f1": f1[opt_index],
-        "phase_threshold": opt_thr,
-    }
-    stats.update(dev_stats)
+        dev_stats = {
+            "dev_phase_precision": prec[opt_index],
+            "dev_phase_recall": recall[opt_index],
+            "dev_phase_f1": f1[opt_index],
+            "phase_threshold": opt_thr,
+        }
+        stats.update(dev_stats)
 
-    prec, recall, f1, _ = precision_recall_fscore_support(
-        test_pred["phase_label_bin"],
-        test_pred["score_p_or_s"] > opt_thr,
-        average="binary",
-    )
-    test_stats = {
-        "test_phase_precision": prec,
-        "test_phase_recall": recall,
-        "test_phase_f1": f1,
-    }
-    stats.update(test_stats)
+        prec, recall, f1, _ = precision_recall_fscore_support(
+            test_pred_restricted["phase_label_bin"],
+            test_pred_restricted["score_p_or_s"] > opt_thr,
+            average="binary",
+        )
+        test_stats = {
+            "test_phase_precision": prec,
+            "test_phase_recall": recall,
+            "test_phase_f1": f1,
+        }
+        stats.update(test_stats)
 
     for pred, set_str in [(dev_pred, "dev"), (test_pred, "test")]:
         for i, phase in enumerate(["P", "S"]):
             pred_phase = pred[pred["phase_label"] == phase]
             pred_col = f"{phase.lower()}_sample_pred"
+
+            if len(pred_phase) == 0:
+                continue
 
             diff = (pred_phase[pred_col] - pred_phase["phase_onset"]) / pred_phase[
                 "sampling_rate"
