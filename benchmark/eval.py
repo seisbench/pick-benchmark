48c07f46ff251b4b9f9378e03b6379ebda2a19e7
Jannes Münchmeyer <munchmej@gfz-potsdam.de>
2021-08-16 13:04:23 +0200

    Fix incorrect merge

--- benchmark/eval.py
+++ benchmark/eval.py
@@ -149,36 +149,6 @@ def _identify_instance_dataset_border(task_targets):
         last_bucket = bucket
 
 
-def load_best_model(model_cls, weights, version):
-    """
-    Determines the model with lowest validation loss from the csv logs and loads it
-
-    :param model_cls: Class of the lightning module to load
-    :param weights: Path to weights as in cmd arguments
-    :param version: String of version file
-    :return: Instance of lightning module that was loaded from the best checkpoint
-    """
-    metrics = pd.read_csv(weights / version / "metrics.csv")
-
-    idx = np.nanargmin(metrics["val_loss"])
-    min_row = metrics.iloc[idx]
-
-    checkpoint = f"epoch={min_row['epoch']:.0f}-step={min_row['step']:.0f}.ckpt"
-
-    version_id = version.split("_")[-1]
-    version_str = f"{version_id}_{version_id}"
-
-    checkpoint_path = (
-        weights.parent
-        / f"{weights.name}_{weights.name}"
-        / version_str
-        / "checkpoints"
-        / checkpoint
-    )
-
-    return model_cls.load_from_checkpoint(checkpoint_path)
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Evaluate a trained model using a set of targets."
